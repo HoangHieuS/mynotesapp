e45646125c718f80dd2c23f5ee4cbddab7340cab
HoangHieu <dinhhoanghieu321@gmail.com>
2022-03-05 17:37:10 +0700

    Cleaning Up Routes

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,5 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'dart:developer' as devtools show log;
+
+import 'package:mynotes/constants/routes.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({Key? key}) : super(key: key);
@@ -51,14 +54,15 @@ class _LoginViewState extends State<LoginView> {
             final email = _email.text;
             final password = _password.text;
             try {
-              final userCredential = await FirebaseAuth.instance
+              await FirebaseAuth.instance
                   .signInWithEmailAndPassword(email: email, password: password);
-              print(userCredential);
+              Navigator.of(context)
+                  .pushNamedAndRemoveUntil(notesRoute, (route) => false);
             } on FirebaseAuthException catch (e) {
               if (e.code == 'user-not-found') {
-                print('User not found');
+                devtools.log('User not found');
               } else if (e.code == 'wrong-password') {
-                print('Wrong password');
+                devtools.log('Wrong password');
               }
             }
           },
@@ -67,7 +71,7 @@ class _LoginViewState extends State<LoginView> {
         TextButton(
             onPressed: () {
               Navigator.of(context)
-                  .pushNamedAndRemoveUntil('/register/', (route) => false);
+                  .pushNamedAndRemoveUntil(registerRoute, (route) => false);
             },
             child: const Text('Not register yet? Register here!'))
       ]),
