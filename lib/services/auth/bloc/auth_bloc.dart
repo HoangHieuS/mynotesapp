de6ae791fe5dd0c5f9a9b51421a4b115bff833d6
HoangHieu <dinhhoanghieu321@gmail.com>
2022-03-15 17:52:12 +0700

    Add forgot password view and padding login & register view

--- lib/services/auth/bloc/auth_bloc.dart
+++ lib/services/auth/bloc/auth_bloc.dart
@@ -6,6 +6,42 @@ import 'package:mynotes/services/auth/bloc/auth_state.dart';
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   AuthBloc(AuthProvider provider)
       : super(const AuthStateUninitialized(isLoading: true)) {
+    //forgot password
+    on<AuthEventForgotPassword>((event, emit) async {
+      emit(const AuthStateForgotPassword(
+        exception: null,
+        hasSentEmail: false,
+        isLoading: false,
+      ));
+      final email = event.email;
+      if (email == null) {
+        return; // user just wants to go to forgot-password screen
+      }
+
+      //user wants to actually send a forgot-password email
+      emit(const AuthStateForgotPassword(
+        exception: null,
+        hasSentEmail: false,
+        isLoading: true,
+      ));
+
+      bool didSendEmail;
+      Exception? exception;
+      try {
+        await provider.sendPasswordReset(toEmail: email);
+        didSendEmail = true;
+        exception = null;
+      } on Exception catch (e) {
+        didSendEmail = false;
+        exception = e;
+      }
+
+      emit(AuthStateForgotPassword(
+        exception: exception,
+        hasSentEmail: didSendEmail,
+        isLoading: false,
+      ));
+    });
     //send email verification
     on<AuthEventSendEmailVerification>((event, emit) async {
       await provider.sendEmailVerification();
@@ -55,7 +91,7 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
         );
       }
     });
-
+    // Login
     on<AuthEventLogIn>((event, emit) async {
       emit(
         const AuthStateLoggedOut(
@@ -101,7 +137,7 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
         );
       }
     });
-
+    //Logout
     on<AuthEventLogOut>((event, emit) async {
       try {
         await provider.logOut();
@@ -119,6 +155,15 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
           ),
         );
       }
+    });
+    //Don't have account
+    on<AuthEventShouldRegister>((event, emit) {
+      emit(
+        const AuthStateRegistering(
+          exception: null,
+          isLoading: false,
+        ),
+      );
     });
   }
 }
